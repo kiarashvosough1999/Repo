a797b8e72905bd77add2d829d459073d00650345
kiarashvosough1999 <vosough.k@gmail.com>
2021-08-11 20:36:19 +0430

    OperationExecutingState implements all needing methods, added support for suspending

--- Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationExecutingState.swift
+++ Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationExecutingState.swift
@@ -7,24 +7,27 @@
 
 import Foundation
 
-class OperationExecutingState: OperationState {
+internal class OperationExecutingState: OperationStateProtocol {
 
-    weak var context: AsynchronousOperation?
-    var isFinished: Bool { false }
-    var isExecuting: Bool { true }
+    internal weak var context: AsynchronousOperation?
+    internal var isFinished: Bool { false }
+    internal var isExecuting: Bool { true }
+    internal var isCanceled: Bool = false
+    internal var isSuspended: Bool = false
+    internal var state: OperationState { .executing }
+    internal var queueState: QueueState
     
-    func completeOperation() throws {
-        guard let context = context else {
-            throw OperationControllerError.dealocatedOperation(
-                """
-                context was dealocated on\(String(describing: self)), cannot change state
-                """
-            )
-        }
-        context.changeState(new: OperationFinishState())
+    internal init(context: AsynchronousOperation? = nil, queueState: QueueState) {
+        self.context = context
+        self.queueState = queueState
     }
     
-    func cancelOperation() throws {
+    /// Complete operation by calling
+    /// 1. `onFinish` block
+    /// 2. Changing state to `OperationFinishState`
+    /// - Parameter execute: onFinish block which was provoded by overriding it on subclasses
+    /// - Throws: OperationControllerError.dealocatedOperation or
+    internal func completeOperation(and execute: OperationCompletedSignal?) throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -32,8 +35,58 @@ class OperationExecutingState: OperationState {
                 """
             )
         }
-        context.changeState(new: OperationCancelState())
+        guard let execute = execute else {
+            throw OperationControllerError.nilBlock(
+                """
+                complete block could not be executed on\(String(describing: self)) while it is nil
+                cannot change state
+                """
+            )
+        }
+        execute()
+        context.changeState(new: OperationFinishState(context: context, enqueued: queueState.enqueued))
+    }
+    
+    func cancelOperation(and execute: OperationCompletedSignal?) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        guard let execute = execute else {
+            throw OperationControllerError.nilBlock(
+                """
+                cancel block could not be executed on\(String(describing: self)) while it is nil
+                cannot change state
+                """
+            )
+        }
+        execute()
+        context.changeState(new: OperationCancelState(context: context, queueState: queueState))
         context.cancel()
+    }
+    
+    #warning("handle suspend after amount of time")
+    func suspend(after: TimeInterval, execute: OperationCompletedSignal?) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        guard let execute = execute else {
+            throw OperationControllerError.nilBlock(
+                """
+                cancel block could not be executed on\(String(describing: self)) while it is nil
+                cannot change state
+                """
+            )
+        }
+        execute()
+        context.changeState(new: OperationSuspendState(context: context, queueState: queueState))
     }
     
 }
