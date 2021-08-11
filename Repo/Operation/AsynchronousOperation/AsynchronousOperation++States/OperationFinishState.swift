62dc88d4c8f7ee4e3ac1fec13c87a5bc87904b87
kiarashvosough1999 <vosough.k@gmail.com>
2021-08-11 20:36:43 +0430

    OperationFinishState implements all needing methods

--- Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationFinishState.swift
+++ Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationFinishState.swift
@@ -7,9 +7,87 @@
 
 import Foundation
 
-class OperationFinishState: OperationState {
+internal class OperationFinishState: OperationStateProtocol {
     
-    func completeOperation() throws {
+    internal weak var context: AsynchronousOperation?
+    internal var isFinished: Bool { true }
+    internal var isExecuting: Bool { false }
+    internal var state: OperationState { .finished }
+    internal var isCanceled: Bool = false
+    internal var isSuspended: Bool = false
+    internal var queueState: QueueState
+    
+    
+    internal init(context: AsynchronousOperation? = nil, enqueued:Bool = false) {
+        self.context = context
+        self.queueState = QueueState(enqueued: enqueued)
+    }
+    
+    func await(after: TimeInterval) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is already finished
+            Could not be awaited
+            """
+        )
+    }
+    
+    func start() throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is already finished
+            Could not be started
+            """
+        )
+    }
+    
+    func suspend(after: TimeInterval, execute: OperationCompletedSignal?) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is already finished
+            Could not be suspended
+            """
+        )
+    }
+    
+    func cancelOperation(and execute: OperationCompletedSignal?) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is already finished
+            Could not be canceled
+            """
+        )
+    }
+    
+    internal func completeOperation() throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -20,11 +98,8 @@ class OperationFinishState: OperationState {
         throw OperationControllerError.operationIsNotExecutingToFinish(
             """
             Operation with identifier: \(context.identifier) is already finished,
+            Could not be finished again
             """
         )
     }
-    
-    weak var context: AsynchronousOperation?
-    var isFinished: Bool { true }
-    var isExecuting: Bool { false }
 }
