46003cdb6ae8a1442c17b50ba3dca051fbd3dfd5
kiarashvosough1999 <vosough.k@gmail.com>
2021-08-11 18:31:50 +0430

    remove with filter and for each item a block execution, <- overloading for adding to dictionary for OperationIdentifier key

--- Repo/Extensions/Dictionary++Extensions.swift
+++ Repo/Extensions/Dictionary++Extensions.swift
@@ -12,4 +12,35 @@ extension Dictionary {
     static func +=(lhs: inout [Self.Key: Self.Value], rhs: [Self.Key: Self.Value]) {
         rhs.forEach { lhs[$0] = $1 }
     }
+    
+    mutating func remove(_ where: (Dictionary<Key, Value>.Element) throws -> Bool) rethrows {
+        try self
+            .filter(`where`)
+            .forEach {
+                self.removeValue(forKey: $0.key)
+            }
+    }
+    
+    mutating func remove(_ where: (Dictionary<Key, Value>.Element) throws -> Bool,
+                         execute onEach: (Dictionary<Key, Value>.Element) throws -> Void) rethrows {
+        try self
+            .filter(`where`)
+            .forEach {
+                try onEach($0)
+                self.removeValue(forKey: $0.key)
+            }
+    }
+    
 }
+
+infix operator <- : AssignmentPrecedence
+extension Dictionary where Key == OperationIdentifier {
+    
+    static func <- <T>(lhs: inout [Self.Key: Self.Value], rhs: T) -> T where T: IdentifiableOperation{
+        lhs.updateValue(rhs as! Self.Value, forKey: rhs.identifier)
+        return rhs
+    }
+}
+
+
+
