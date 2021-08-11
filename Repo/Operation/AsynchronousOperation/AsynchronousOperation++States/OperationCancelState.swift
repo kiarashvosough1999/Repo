de3fd664f4f74484e8c16112dda9a2617d873034
kiarashvosough1999 <vosough.k@gmail.com>
2021-08-11 20:35:46 +0430

    OperationCancelState implements all needing methods

--- Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationCancelState.swift
+++ Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationCancelState.swift
@@ -7,9 +7,47 @@
 
 import Foundation
 
-class OperationCancelState: OperationState {
+internal class OperationCancelState: OperationStateProtocol {
     
-    func completeOperation() throws {
+    internal weak var context: AsynchronousOperation?
+    internal var isFinished: Bool { true }
+    internal var isExecuting: Bool { false }
+    internal var state: OperationState { .canceled }
+    internal var isCanceled: Bool = true
+    internal var isSuspended: Bool = false
+    internal var queueState: QueueState
+    
+    
+    internal init(context: AsynchronousOperation? = nil, queueState: QueueState) {
+        self.context = context
+        self.queueState = queueState
+    }
+    
+    func start() throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        try queueState.willEnqueue.accept {
+            throw OperationControllerError.cancelDuringWaitingForDeadline(
+                """
+                Operation with identifier: \(context.identifier) was canceled
+                when it was waiting to be enqueued after its \(queueState.enqueuedAfter) sec deadline.
+                """
+            )
+        }
+        throw OperationControllerError.operationAlreadyCanceled(
+            """
+            Operation with identifier: \(context.identifier) was canceled,
+            and could not be started again
+            """
+        )
+    }
+    
+    func suspend(after: TimeInterval, execute: OperationCompletedSignal?) throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -19,12 +57,41 @@ class OperationCancelState: OperationState {
         }
         throw OperationControllerError.operationIsNotExecutingToFinish(
             """
-            Operation with identifier: \(context.identifier) is already canceled and finished,
+            Operation with identifier: \(context.identifier) is already canceled,
+            and could not be suspended.
             """
         )
     }
     
-    weak var context: AsynchronousOperation?
-    var isFinished: Bool { true }
-    var isExecuting: Bool { true }
+    func await(after: TimeInterval) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is already canceled,
+            and could not be awaited.
+            """
+        )
+    }
+    
+    internal func completeOperation() throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is already canceled,
+            and could not be completed.
+            """
+        )
+    }
 }
--- Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationReadyState.swift
+++ Repo/Operation/AsynchronousOperation/AsynchronousOperation++States/OperationReadyState.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
-class OperationReadyState: OperationState {
+internal class OperationReadyState: OperationStateProtocol {
     
-    weak var context: AsynchronousOperation?
+    internal weak var context: AsynchronousOperation?
+    internal var isFinished: Bool { false }
+    internal var isExecuting: Bool { false }
+    internal var isCanceled: Bool = false
+    internal var isSuspended: Bool = false
+    internal var state: OperationState { .ready }
+    internal var queueState:QueueState
     
-    var isFinished: Bool { false }
-    var isExecuting: Bool { false }
-    
-    private var enqueued = false
-    
-    init(context: AsynchronousOperation?) {
+    internal init(context: AsynchronousOperation? = nil, queueState:QueueState) {
         self.context = context
+        self.queueState = queueState
     }
     
-    func cancelOperation() throws {
+    internal func cancelOperation(and execute: OperationCompletedSignal?) throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -28,10 +30,15 @@ class OperationReadyState: OperationState {
                 """
             )
         }
-        context.cancel()
+        context.changeState(new: OperationCancelState(context: context, queueState: queueState))
     }
     
-    func start() throws {
+    /// First the `await` method should be called  then
+    /// This method will be called on overided method `start` (when OperationQueue attemp to call it)
+    /// on operation subclass in order to start task and change its state
+    /// `enqueued.enqueued` is `true` on this state
+    /// - Throws: Throws OperationControllerError.dealocatedOperation if the context is nil
+    internal func start() throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -39,11 +46,12 @@ class OperationReadyState: OperationState {
                 """
             )
         }
-        context.changeState(new: OperationExecutingState())
+        // handle complete and cancel event when deadline is more than 0.0 and operation is waiting to be enqueued
+        context.changeState(new: OperationExecutingState(context: context, queueState: queueState))
         context.main()
     }
     
-    func completeOperation() throws {
+    internal func completeOperation(and execute: OperationCompletedSignal?) throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -51,17 +59,16 @@ class OperationReadyState: OperationState {
                 """
             )
         }
-        try isExecuting.reject {
-            throw OperationControllerError.operationIsNotExecutingToFinish(
-                """
-                Operation with identifier: \(context.identifier) is not executing to finish,
-                try to cancel it before execution
-                """
-            )
-        }
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is not executing to complete.
+            only executing operation can be completed with `completeOperation` method.
+            try to cancel if you want to prevent it from execution.
+            """
+        )
     }
     
-    func await(after:TimeInterval = 0) throws {
+    func suspend(after: TimeInterval, execute: OperationCompletedSignal?) throws {
         guard let context = context else {
             throw OperationControllerError.dealocatedOperation(
                 """
@@ -69,22 +76,53 @@ class OperationReadyState: OperationState {
                 """
             )
         }
-        guard !enqueued  else {
+        throw OperationControllerError.operationIsNotExecutingToFinish(
+            """
+            Operation with identifier: \(context.identifier) is not executing to suspend.
+            only executing operation can be suspended with `suspendOperation` method.
+            try to cancel if you want to prevent it from execution.
+            """
+        )
+    }
+    
+    /// Each time calling `await` on an operation will result in async adding  the operation to `OperationQueue` within its `underlyingQueue`
+    /// This method should not be called when operation is already on a queue
+    /// The state of the operation won't be afected until `start` triggering
+    /// `start` method will be called ofter the deadline
+    /// - Parameter after: amount of time to tolerate until the operation will be added
+    /// - Throws: Throws OperationControllerError.dealocatedOperation if the context is nil
+    internal func await(after:TimeInterval = 0) throws {
+        guard let context = context else {
+            throw OperationControllerError.dealocatedOperation(
+                """
+                context was dealocated on\(String(describing: self)), cannot change state
+                """
+            )
+        }
+        guard !queueState.enqueued  else {
             throw OperationControllerError.misUseError(
                 "operation with identifier \(context.identifier) is already enqueued on a queue"
             )
         }
+        
+        if queueState.enqueued {
+            context
+                .changeState(new: OperationExecutingState(context: context,
+                                                          queueState: queueState))
+            return
+        }
+        
         try context.operationQueue.on { operationQueue in
             try operationQueue.underlyingQueue.on { queue in
-                queue.asyncAfter(deadline: .now() + after) {
-                    [weak self] in
-                        guard let self = self else {
-                            fatalError(
-                                "State \(String(describing: self)) was dealocated"
-                            )
-                        }
-                        operationQueue.addOperation(context)
-                        self.enqueued = true
+                self.queueState.enqueuedAfter = after
+                queue.asyncAfter(deadline: .now() + after) { [weak self] in
+                    guard let self = self else {
+                        fatalError(
+                            "State \(String(describing: self)) was dealocated"
+                        )
+                    }
+                    operationQueue.addOperation(context)
+                    self.queueState.enqueued = true
                 }
             } none: {
                 throw OperationControllerError.operationQueueIsNil(
@@ -94,7 +132,7 @@ class OperationReadyState: OperationState {
                         was found nil,can not add this operation to execute
                     """)
             }
-//            context.changeState(new: OperationExecutingState())
+            //            context.changeState(new: OperationExecutingState())
         } none: {
             throw OperationControllerError.operationQueueIsNil(
                 """
@@ -104,8 +142,10 @@ class OperationReadyState: OperationState {
         }
         // block the current thread until all operation finish
         // new operation cant be added to queue during this block time
-        context.operationConfiguration
-            .waitUntilAllOperationsAreFinished.accept {
+        context
+            .operationConfiguration
+            .waitUntilAllOperationsAreFinished
+            .accept {
                 context.operationQueue?.waitUntilAllOperationsAreFinished()
             }
     }
