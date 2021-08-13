e325614a80a3053ae785782eb5749dbb6f4b3e98
kiarashvosough1999 <vosough.k@gmail.com>
2021-08-13 18:15:46 +0430

    test cases class

---
+++ Repo/Extensions/Queue.swift
@@ -0,0 +1,32 @@
+//
+//  Queue.swift
+//  Repo
+//
+//  Created by Kiarash Vosough on 5/21/1400 AP.
+//
+
+import Foundation
+
+struct Queue<T> {
+    
+    private var elements: [T] = []
+    
+    mutating func enqueue(_ value: T) {
+        elements.append(value)
+    }
+    
+    mutating func dequeue() -> T? {
+        guard !elements.isEmpty else {
+            return nil
+        }
+        return elements.removeFirst()
+    }
+    
+    var head: T? {
+        return elements.first
+    }
+    
+    var tail: T? {
+        return elements.last
+    }
+}
--- Repo/ViewController.swift
+++ Repo/ViewController.swift
@@ -8,32 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var c:Router<health>? = Router<health>()
+    
+    @OpenSession(2) var sessionController = SessionController<health>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        print("passed")
-        c?.request(.foods) { data, resp, error in
-            print("400")
-            print(String(data: data!, encoding: .utf8)?.prefix(400))
-            return
-        }
-        print("passed7")
-        c?.request(.foods) { data, resp, error in
-            print("500")
-            print(String(data: data!, encoding: .utf8)?.prefix(500))
-            return
-        }
-        c?.request(.foods) { data, resp, error in
-            print("200")
-            print(String(data: data!, encoding: .utf8)?.prefix(200))
-            return
+        
+        let builder = URLHTTPRequestBuilder<health>()
+        print("here")
+        do {
+            let x = try sessionController.downloadTask(with: .act, completionHandler: { url, _, _ in
+                print(url)
+            }).changeOperationConfig({
+                $0.identifierGenerator = { .download }
+            }).await(after: 0)
+            
+            let x6 = try sessionController.dataTask(with: .foods,
+                                                    requestBuilder: builder,
+                                                    completionHandler: { data,_,_ in
+                                                        print(100)
+                                                        print(String(data: data!, encoding: .utf8)?.prefix(100))
+                                                        do {
+                                                            try x.suspend(after: 5)
+                                                        }catch {
+                                                            print(error)
+                                                        }
+                                                    }).await(after: 0)
+            
+            let x8 = try sessionController.dataTask(with: .foods,
+                                                    requestBuilder: builder,
+                                                    completionHandler: {data,_,_ in
+                                                        print(200)
+                                                        print(String(data: data!, encoding: .utf8)?.prefix(200))
+                                                        do {
+                                                            try x.await(after: 13)
+                                                        }catch {
+                                                            print(error)
+                                                        }
+                                                    })
+                .await(after: 0)
+            
+        }catch {
+            print(error)
         }
         
     }
-
-
+    
+    
 }
 
