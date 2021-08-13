d96995c8e2e9e0c4124c2997c9e2b6e6fa15bc1f
kiarashvosough1999 <vosough.k@gmail.com>
2021-08-13 18:10:07 +0430

    OperationController added again to make some repeatable properties abstract

---
+++ Repo/Operation/NetworkOperationController/OperationController.swift
@@ -0,0 +1,177 @@
+//
+//  OperationController.swift
+//  Repo
+//
+//  Created by Kiarash Vosough on 5/21/1400 AP.
+//
+
+import Foundation
+
+public class OperationController<Task,Configuration>: AsynchronousOperation,
+                                                      TaskOperationControllerProtocol where Task:URLSessionTask,
+                                                                                            Configuration:URLSessionAnyTaskConfigurationProtocol{
+    
+    public typealias SessionTask = Task
+    
+    public typealias TaskConfiguration = Configuration
+    
+    internal private(set) var taskConfiguration: Configuration
+    
+    internal var task: SessionTask?
+    
+    private var autoUpdateTaskConfigOnChange: Bool
+    
+    @available(iOS 7.0, *)
+    @objc public var progress: Progress? { task?.progress }
+    
+    override var onCanceled: StateFullOperation.WorkerItemBlock? {
+        return {
+            .init(dispathOption: .asyncWithInheritedQueue) { [weak self] in
+                guard let self = self else { fatalError("Unable to execute cancel block") }
+                self.task?.cancel()
+                self.task.toggleNil()
+                
+            }
+        }
+    }
+    
+    override var onFinished: StateFullOperation.WorkerItemBlock? {
+        return {
+            .init(dispathOption: .asyncWithInheritedQueue) { [weak self] in
+                guard let self = self else { fatalError("Unable to execute finish block") }
+                self.task?.cancel()
+                self.task.toggleNil()
+            }
+        }
+    }
+    
+    override var onExecuting: StateFullOperation.WorkerItemBlock? {
+        return {
+            .init(dispathOption: .asyncWithInheritedQueue) { [weak self] in
+                guard let self = self else { fatalError("Unable to execute executing block") }
+                self.task?.resume()
+            }
+        }
+    }
+    
+    
+    init(operationQueue: OperationQueue,
+         sessionTask: @autoclosure SessionTaskBlock,
+         autoUpdateTaskConfigOnChange: Bool = true,
+         taskConfig:Configuration = .init(),
+         operationConfig: OperationConfig = .init()) {
+        self.autoUpdateTaskConfigOnChange = autoUpdateTaskConfigOnChange
+        self.task = sessionTask()
+        self.taskConfiguration = taskConfig
+        super.init(operationQueue: operationQueue,
+                   operationConfiguration: operationConfig)
+    }
+    
+    
+    @discardableResult
+    public func applyTaskConfiguration() -> Self {
+        self.taskDescription = taskConfiguration.taskDescription
+        if #available(iOS 11.0, *) {
+            self.earliestBeginDate = taskConfiguration.earliestBeginDate
+            self.countOfBytesClientExpectsToSend = taskConfiguration.countOfBytesClientExpectsToSend
+            self.countOfBytesClientExpectsToReceive = taskConfiguration.countOfBytesClientExpectsToReceive
+        }
+        if #available(iOS 14.5, *) {
+            self.prefersIncrementalDelivery = taskConfiguration.prefersIncrementalDelivery
+        }
+        if #available(iOS 8.0, *) {
+            self.priority = taskConfiguration.priority
+        }
+        return self
+    }
+    
+    @discardableResult
+    public func changeTaskConfiguration(_ config: (inout Configuration) -> Void) -> Self {
+        config(&self.taskConfiguration)
+        if autoUpdateTaskConfigOnChange { applyTaskConfiguration() }
+        return self
+    }
+    
+}
+
+extension OperationController {
+    
+    public var taskDescription: String? {
+        get {
+            task?.taskDescription
+        }
+        set {
+            task?.taskDescription = newValue
+        }
+    }
+    
+    public var taskState: URLSessionTask.State? { task?.state }
+    
+    public var taskIdentifier: Int? { task?.taskIdentifier }
+    
+    public var originalRequest: URLRequest? { task?.originalRequest }
+    
+    public var currentRequest: URLRequest? { task?.currentRequest }
+    
+    public var response: URLResponse? { task?.response }
+    
+    @available(iOS 11.0, *)
+    public var earliestBeginDate: Date? {
+        get {
+            task?.earliestBeginDate
+        }
+        set {
+            task?.earliestBeginDate = newValue
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    public var countOfBytesClientExpectsToSend: Int64? {
+        get {
+            task?.countOfBytesClientExpectsToSend
+        }
+        set {
+            task?.countOfBytesClientExpectsToSend = newValue.or(NSURLSessionTransferSizeUnknown)
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    public var countOfBytesClientExpectsToReceive: Int64? {
+        get {
+            task?.countOfBytesClientExpectsToReceive
+        }
+        set {
+            task?.countOfBytesClientExpectsToReceive = newValue.or(NSURLSessionTransferSizeUnknown)
+        }
+    }
+    
+    public var countOfBytesReceived: Int64? { task?.countOfBytesReceived }
+    
+    public var countOfBytesSent: Int64? { task?.countOfBytesSent }
+    
+    public var countOfBytesExpectedToSend: Int64? { task?.countOfBytesExpectedToSend }
+    
+    public var countOfBytesExpectedToReceive: Int64? { task?.countOfBytesExpectedToReceive }
+    
+    public var error: Error? { task?.error }
+    
+    @available(iOS 8.0, *)
+    public var priority: Float? {
+        get {
+            task?.priority
+        }
+        set {
+            task?.priority = newValue.or(URLSessionTask.defaultPriority)
+        }
+    }
+    
+    @available(iOS 14.5, *)
+    public var prefersIncrementalDelivery: Bool? {
+        get {
+            task?.prefersIncrementalDelivery
+        }
+        set {
+            task?.prefersIncrementalDelivery = newValue.or(false)
+        }
+    }
+}
